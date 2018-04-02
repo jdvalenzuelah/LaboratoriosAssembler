b764ecf63b476b950023c548aea287e100f074e4
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-02 09:16:29 -0600

    8

--- LAB8/tem5.s
+++ LAB8/tem5.s
@@ -9,7 +9,10 @@ Autor: David Valenzuela 171001
 
 @Datos
 .data
-string: .asciz "El resultado es: %d\n" @String a imprimir
+string: .asciz "EL resultado es: %d\n"
+num1: .word 7
+num2: .word -1
+num3: .word 5 
 
 /* Funcion main del programa*/
 .text
@@ -17,17 +20,41 @@ string: .asciz "El resultado es: %d\n" @String a imprimir
 .extern printf @printf de la libreria de c para imprimir
 main:
 	
-	.ORIG x3000
+
         push {ip, lr} @ip y lr son añadidos al stack
 
+        /*Cargamos los datos*/
         ldr r0, =string
-        mov r5, pc
-        add r5, r5, #0
-        bl printf
+        ldr r2, adrn1 @num1
+        ldr r3, adrn2 @num2
+        ldr r4, adrn3 @num3
+        ldr r2, [r2] @num1
+        ldr r3, [r3] @num2
+        ldr r4, [r4] @num3
+        mov r1, #0 @Contador numeros negativos
+        mov r5, #0 @Numero actual
 
-       /* ldr r0, =x3030
-        ldr r1, =x3031
-        ldr r2, =x3032*/
+        /*Operaciones*/
+
+        cmp r2, #0 @if (r2 <= 0)
+        movle r5, #1 @ r5 = 1
+        movgt r5, #0 @ else: r5 = 0
+        add r1, r1, r5 @ r1 = r1 + r5
+
+        cmp r3, #0 @if (r3 <= 0)
+        movle r5, #1 @r5 = 1
+        movgt r5, #0 @else: r5 = 0
+        add r1, r5 @r1 = r1 + r5
+
+        cmp r4, #0 @if(r4 <= 0)
+        movle r5, #1@r5 = 1
+        movgt r5, #0 @else: r5 = 0
+        add r1, r5 @r1 = r1 +r5
+
+        bl printf @Imprimir el resultado
 
         pop {ip, pc} @pop del ip y pc al stack
 
+adrn1: .word num1
+adrn2: .word num2
+adrm3: .word num3
