98eec1c801a04995bd9a4a3f43dcb2e879f67afc
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-04-09 07:31:01 -0600

    main

--- LAB9/calculadora.s
+++ LAB9/calculadora.s
@@ -8,3 +8,12 @@ Organizacion de computadoras y assembler
 .align 2
 menu: .asciz "Ingrese una opcion a trabjar. \n	+ para suma \n	- para resta \n	* para multiplicacion \n	= para mostrar el resultado almacenado \n	q salir del programa"
 
+/*Funcion principal el programa*/
+.text
+.global main
+.align , %function
+.extern
+.type main, printf
+
+main: 
+	stmfd sp!, {lr} /*link register*/
