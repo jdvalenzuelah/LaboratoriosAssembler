b9074bb63321eb8f353ee9e43755a066b5f0f46b
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-09 16:29:11 -0600

    Prueba

--- LAB9/calculadora.s
+++ LAB9/calculadora.s
@@ -3,72 +3,31 @@ Universidad del Valle de Guatemala
 Organizacion de computadoras y assembler
 */
 
-/* Datos con lo que se trabajara */
+/* ---------------- Datos con lo que se trabajara ---------------- */
 .data
 .align 2
 
-a: .asciz "%s"
-b: .asciz "%d"
-
 /* Formatos para ingreso de datos */
 menu: .asciz "Ingrese una opcion a trabjar: \n	+ para suma \n	- para resta \n	* para multiplicacion \n	= para mostrar el resultado almacenado \n	q salir del programa\n"
-opcion: .asciz "%s"
-operando: .string "%d"
-/* Operandos */
-suma: .string "+"
-resta: .string "-"
-multiplicacion: .string "*"
-salir: .string "q"
 
-valor: .word 0
 
-/*Funcion principal el programa*/
+/* --------------------------------------------------------------- */
+
+/*  ------------------ Funcion main del programa ----------------- */
 .text
 .global main
-.align 2
-.type main, %function
-.extern printf, scanf
+.extern printf @printf de la libreria de c para imprimir
+main:
+	stmfd sp!, {lr}	/* SP = R13 link register */
 
+	/* Imprimimos el menu */
+	ldr r0, =menu @Cargamos el menu a r0
+	bl printf @Imprimimos el menu
 
-/* Funcion para suma de datos*/
-sumar:
-	ldr r4, adrvalor
-	ldr r4, [r4]
-	add r6, r4, r5
-	str r4, [r6]
-	b main
-
-
-main: 
-	stmfd sp!, {lr} /*link register*/
-
-	ldr r0, =menu
-	bl printf
-
-	@Seleccion de opcion del programa
-	ldr r0, =a
-	ldr r1, =opcion
-	bl scanf
-
-	/* cargamos la opcion */
-	ldr r0, =opcion
-	bl printf
-
-	ldr r0, =suma
-	ldr r0, [r0]
-	ldr r1, =opcion
-	ldr r1, [r1]
-
-	/* Opcion de suma */
-	cmp r0, r1
-	ldreq r0, =menu
-	bleq printf
-
-salida:
 	/* salida correcta */
 	mov r0, #0
 	mov r3, #0
 	ldmfd sp!, {lr}	/* R13 = SP */
 	bx lr
 
-adrvalor: .word valor
+/* --------------------------------------------------------------- */
