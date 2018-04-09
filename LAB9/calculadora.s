86ac48e104da2c42979527ec987532f198b61635
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-09 07:29:42 -0600

    Menu

--- LAB9/calculadora.s
+++ LAB9/calculadora.s
@@ -1,2 +1,10 @@
 /*
+Universidad del Valle de Guatemala
+Organizacion de computadoras y assembler
 */
+
+/* Datos con lo que se trabajara */
+.data
+.align 2
+menu: .asciz "Ingrese una opcion a trabjar. \n	+ para suma \n	- para resta \n	* para multiplicacion \n	= para mostrar el resultado almacenado \n	q salir del programa"
+
