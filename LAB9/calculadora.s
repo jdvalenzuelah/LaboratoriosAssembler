6f4ccb39d0e53f52811bb2ea1e9675a61fb9d71d
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-04-09 08:16:59 -0600

    ...

--- LAB9/calculadora.s
+++ LAB9/calculadora.s
@@ -13,6 +13,7 @@ b: .asciz "%s"
 menu: .asciz "Ingrese una opcion a trabjar: \n	+ para suma \n	- para resta \n	* para multiplicacion \n	= para mostrar el resultado almacenado \n	q salir del programa\n"
 opcion: .asciz "%s"
 operando: .asciz "%d"
+valor: .word 0
 /*Funcion principal el programa*/
 .text
 .global main
@@ -43,3 +44,10 @@ main:
 	mov r3, #0
 	ldmfd sp!, {lr}	/* R13 = SP */
 	bx lr
+
+suma:
+	ldr r4, =a
+	ldr r0, [r4]	/*guardamos el valor de a en el registro r4*/
+	add r5, r4, r4
+	ldr r4, = valor
+	str r5, [r4]
