e46720d9332ba5e51a942392891b10ecb4cb7ccb
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-16 09:15:40 -0600

    lll

--- LAB10/lab10programa2.s
+++ LAB10/lab10programa2.s
@@ -8,7 +8,7 @@ autor: Juan Jose Navas
 .data
 .align 2
 mensaje: .asciz "Ingrese nombre en minusculas (Maximo 10 caracteres, y sin espacios en blaco)\n"
-formatoEntrada: .asciz "%s"
+formatoEntrada: .string "%s"
 nombre: .string "%s"
 resultado: .asciz " "
 
