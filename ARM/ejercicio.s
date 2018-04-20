af0317812ea0b7ae1f20a38c4165a6fa2c96fbdc
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-20 07:59:16 -0600

    sadf

---
+++ ARM/ejercicio.s
@@ -0,0 +1,31 @@
+.data
+.align 2
+salida: .asciz "Promedio es: %d\n"
+
+.text
+.align 2
+.global main
+.type main,%function
+
+main:
+	stmfd sp!,{lr}
+
+
+	@Salida
+	mov r0,#0
+	mov r3,#0
+	ldmfd sp!,{lr}
+	bx lr
+
+
+.global promedio
+promedio:
+	add r0, r0, r1
+	lsr r0, #1
+	push{r0, lr}
+	mov r1, r0
+	ldr r0, =salida
+	bl printf
+	pop{r0, lr}
+	mov pc, lr
+
