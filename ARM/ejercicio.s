13f7e559e71387dabbb9e6ace1a1866cded13143
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-20 08:02:05 -0600

    asdf

--- ARM/ejercicio.s
+++ ARM/ejercicio.s
@@ -17,15 +17,15 @@ main:
 	ldmfd sp!,{lr}
 	bx lr
 
-
+/* Promedio de 2 numeros */
 .global promedio
 promedio:
-	add r0, r0, r1
-	lsr r0, #1
-	push{r0, lr}
-	mov r1, r0
-	ldr r0, =salida
-	bl printf
-	pop{r0, lr}
-	mov pc, lr
+	add r0, r0, r1 @Sumamos todos los numeros
+	lsr r0, #1 @Dividimos dentro de 2
+	push {r0, lr} @Push del resultado y el lr
+	mov r1, r0 @Trasladamos el resultado a r1
+	ldr r0, =salida @Cargamos en r0 el formaro de salida
+	bl printf @Imprimimos el resultado
+	pop {r0, lr} @Restauramos los valores de r0 y lr
+	mov pc, lr @Retornamos
 
