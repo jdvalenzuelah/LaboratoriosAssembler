553415d1251378780f136d9336f4149d736a44e8
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-23 14:03:00 -0600

    main

---
+++ LAB11/main.s
@@ -0,0 +1,57 @@
+/**********************************************************************
+Universidad del Valle de Guatemala
+Authors: Josue David Valenzuela 171001
+		 Marcos Gutierrez	    17909
+Two player connect 4 game developed as part of CC3055 course
+File contains subroutines and stores the data for the connect 4 game.
+**********************************************************************/
+.data
+inputFormat: .asciz "%d"
+inputMessage: .asciz "Ingrese un numero:\n"
+num1: .word 0
+num2: .word 0
+num3: .word 0
+num4: .word 0
+
+/* main function */
+.text
+.align 2
+.global main
+.type main,%function
+main:
+	stmfd sp!,{lr}
+
+	ldr r0, =inputMessage
+	bl printf
+	ldr r0, =inputFormat
+	ldr r1, =num1
+	bl scanf
+
+	ldr r0, =inputMessage
+	bl printf
+	ldr r0, =inputFormat
+	ldr r1, =num2
+	bl scanf
+
+	ldr r0, =inputMessage
+	bl printf
+	ldr r0, =inputFormat
+	ldr r1, =num3
+	bl scanf
+
+	ldr r0, =inputMessage
+	bl printf
+	ldr r0, =inputFormat
+	ldr r1, =num4
+	bl scanf
+
+/* exit code */
+exit:
+	@UN LINK variables
+	.unreq winner
+	.unreq cont
+	@OS exit
+	mov r0,#0
+	mov r3,#0
+	ldmfd sp!,{lr}
+	bx lr
