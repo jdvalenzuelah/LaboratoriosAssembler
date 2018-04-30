baf02f9246428f058821e086802b5ecf0577deec
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-30 13:57:48 -0600

    df

--- LAB11/s.s
+++ LAB11/s.s
@@ -69,5 +69,6 @@ CalculoNotaProyecto:
 	@Encontramos cuanto falta para llegar a 61
 	vsub.F64 d5, d9, d5
 	@Guardamos el resultado en la direccion de r0
+	ldr r0, =nota1
 	vstr d5, [r0]
 	mov pc, lr @Return r0
