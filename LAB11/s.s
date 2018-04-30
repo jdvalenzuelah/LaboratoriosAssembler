f6027c15b5784f7b54b829eb94734d379b725214
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-30 13:56:07 -0600

    asd

--- LAB11/s.s
+++ LAB11/s.s
@@ -20,10 +20,10 @@ main:
 	stmfd sp!, {lr}	/* SP = R13 link register */
 
 	@Cargamos las notas a los registros
-	ldr r0, =addrNota1
-	ldr r1, =addrNota2
-	ldr r2, =addrNota3
-	ldr r3, =addrNota4
+	ldr r0, addrNota1
+	ldr r1, addrNota2
+	ldr r2, addrNota3
+	ldr r3, addrNota4
 
 	bl CalculoNotaProyecto @Llamamos a la subrutina
 	@valor de retorno esta en r0, lo movemos a r1
@@ -50,19 +50,24 @@ addrNota4: .word nota4
 */
 .global CalculoNotaProyecto
 CalculoNotaProyecto:
-	@Cargar los valores punto flotante
+	@Cargar los valores punto flotante y convertirlos a B64
 	vldr s0, [r0]
+	vcvt.F64.F32 d5, s0
 	vldr s1, [r1]
+	vcvt.F64.F32 d6, s1
 	vldr s2, [r2]
+	vcvt.F64.F32 d7, s2
 	vldr s3, [r3]
+	vcvt.F64.F32 d8, s3
 	mov r5, #61
-	@S4 se usa para guardar la suma de las notas
-	vadd s4, s0, s1 @s4 = s0 + s1
-	vadd s4, s4, s2 @s4 = s4 + s2
-	vadd s4, s4, s3 @s4 = s4 + s3
-	@busca32lo restante para llegar a 61
-	vmov s5, r5 @d5 = 61
-	vsub.F32 s4, s5, s4 @d4 = d5 - d4
-	@Guardar la direccion en r0
-	vstr s4, [r0]
+	vmov s4, r5
+	vcvt.F64.F32 d9, s4
+	@Sumamos todos los valores
+	vadd.F64 d5, d5, d6
+	vadd.F64 d5, d5, d7
+	vadd.F64 d5, d5, d8
+	@Encontramos cuanto falta para llegar a 61
+	vsub.F64 d5, d9, d5
+	@Guardamos el resultado en la direccion de r0
+	vstr d5, [r0]
 	mov pc, lr @Return r0
