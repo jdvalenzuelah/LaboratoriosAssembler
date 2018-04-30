01fe9c4c8098b4326fc6300c30fd8de48abbc4e3
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-30 14:07:04 -0600

    dsf

--- LAB11/s.s
+++ LAB11/s.s
@@ -29,7 +29,8 @@ main:
 	ldr r2, addrNota3
 	ldr r3, addrNota4
 
-	bl CalculoNotaProyecto @Llamamos a la subrutina
+	bl calculoNotaProyecto @Llamamos a la subrutina
+
 	@valor de retorno esta en r0, lo movemos a r1
 	ldr r1, [r0]
 	@Imprimimos el resultado
@@ -48,33 +49,3 @@ addrNota1: .word nota1
 addrNota2: .word nota2
 addrNota3: .word nota3
 addrNota4: .word nota4
-
-/**
- * r0 - r3 contiene la direccion del valor float
-*/
-.global CalculoNotaProyecto
-CalculoNotaProyecto:
-	push {lr}
-	@Cargar los valores punto flotante y convertirlos a B64
-	vldr s0, [r0]
-	vcvt.F64.F32 d5, s0
-	vldr s1, [r1]
-	vcvt.F64.F32 d6, s1
-	vldr s2, [r2]
-	vcvt.F64.F32 d7, s2
-	vldr s3, [r3]
-	vcvt.F64.F32 d8, s3
-	mov r5, #61
-	vmov s4, r5
-	vcvt.F64.F32 d9, s4
-	@Sumamos todos los valores
-	vadd.F64 d5, d5, d6
-	vadd.F64 d5, d5, d7
-	vadd.F64 d5, d5, d8
-	@Encontramos cuanto falta para llegar a 61
-	vsub.F64 d5, d9, d5
-	ldr r0, =test
-	vmov r2, r3, d5
-	@Guardamos el resultado en la direccion de r0
-	pop {lr}
-	mov pc, lr @Return r0
