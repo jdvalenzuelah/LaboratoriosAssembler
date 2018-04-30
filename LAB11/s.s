d6bd18faa8c7e863dc049e68082fa88fe505f304
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-04-30 08:25:28 -0600

    d

--- LAB11/s.s
+++ LAB11/s.s
@@ -59,8 +59,8 @@ CalculoNotaProyecto:
 	vadd.F32 s4, s4, s2 @s4 = s4 + s2
 	vadd.F32 s4, s4, s3 @s4 = s4 + s3
 	@buscar lo restante para llegar a 61
-	vmov s5, #61
-	vsub s4, s4, s5 @s4 = 61 - s4
+	vmov r5, #61
+	vsub s4, s4, r5 @s4 = 61 - s4
 	vmov r0, s4 @ move the result to r0
 	mov pc, lr @Return r0
 
