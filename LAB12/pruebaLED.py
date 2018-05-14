33db61866d3ebb551574ff636706c0d3c21a9c34
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-05-14 15:49:19 -0600

    ñlkajsdf

--- LAB12/pruebaLED.py
+++ LAB12/pruebaLED.py
@@ -1,16 +1,16 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 #Configurar los pines como saldia
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setwarnings(False)
-#GPIO.setup(16, GPIO.OUT)
-#GPIO.setup(20, GPIO.OUT)
-#GPIO.setup(21, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
 
 #Comenzamos con todos apagados
-#GPIO.output(16, GPIO.LOW)
-#GPIO.output(20, GPIO.LOW)
-#GPIO.output(21, GPIO.LOW)
+GPIO.output(16, GPIO.LOW)
+GPIO.output(20, GPIO.LOW)
+GPIO.output(21, GPIO.LOW)
 
 #Inciamos un contador
 cont = 0
@@ -20,19 +20,19 @@ while(cont < 4):
 	trash = raw_input("Presione una tecla")
 	cont += 1
 	if(cont == 1):
-		#GPIO.output(16, GPIO.HIGH)
-		#GPIO.output(20, GPIO.LOW)
-		#GPIO.output(21, GPIO.LOW)
+		GPIO.output(16, GPIO.HIGH)
+		GPIO.output(20, GPIO.LOW)
+		GPIO.output(21, GPIO.LOW)
 		print("100")
 	if(cont == 2):
-		#GPIO.output(16, GPIO.LOW)
-		#GPIO.output(20, GPIO.HIGH)
-		#GPIO.output(21, GPIO.LOW)
+		GPIO.output(16, GPIO.LOW)
+		GPIO.output(20, GPIO.HIGH)
+		GPIO.output(21, GPIO.LOW)
 		print("010")
 	if(cont == 3):
-		#GPIO.output(16, GPIO.LOW)
-		#GPIO.output(20, GPIO.HIGH)
-		#GPIO.output(21, GPIO.LOW)
+		GPIO.output(16, GPIO.LOW)
+		GPIO.output(20, GPIO.HIGH)
+		GPIO.output(21, GPIO.LOW)
 		print("001")
 		cont = 0
 
