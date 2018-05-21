0d35d4e3b157d8c9e7f5f10034d17c80c6008450
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-05-20 22:53:05 -0600

    GPIO

--- LAB14/main.s
+++ LAB14/main.s
@@ -9,7 +9,6 @@ ingresoSecs: .asciz "Ingrese segundos en la alarma (0-60):\n"
 errorMessageOpt: .asciz "Error! ingrese una opcion valida.\n"
 errorMessageRj: .asciz "Valor invalido! Ingrese un valir entre 0 y 60.\n"
 inputFormat: .asciz "%d"
-testStr:.asciz "Digits: %d - %d \n"
 opt: .word 0
 secs: .word 0
 .global myloc
@@ -21,6 +20,83 @@ myloc: .word 0
 .type main,%function
 main:
 	stmfd sp!,{lr}
+confPin:
+/*
+Configuracion de pines:
+	Pines display 1 utilizando BCD:
+		GPIO 02 --> 1000 A
+		GPIO 03 --> 0100 B
+		GPIO 04 --> 0010 C
+		GPIO 17 --> 0001 D
+	Pines display 2 utilizando BCD:
+		GPIO 22 --> 1000 A
+		GPIO 10 --> 0100 B
+		GPIO 09 --> 0010 C
+		GPIO 11 --> 0001 D
+	Pin LED/Buzzer alarma:
+		GPIO 12
+	Pin Boton aumentar digito:
+		GPIO 5
+	Pin Boton cambiar unidades/decenas:
+		GPIO 6
+	Pin Boton reset:
+		GPIO 13
+*/
+	bl getGpioAddress
+	/* ---- Configurar pines entrada/salida ---- */
+	/* Salidas */
+	@Display 1
+	@@GPIO para escritura puerto 2
+	mov r0,#2
+	mov r1,#1
+	bl SetGpioFunction
+	@@GPIO para escritura puerto 3
+	mov r0,#3
+	mov r1,#1
+	bl SetGpioFunction
+	@@GPIO para escritura puerto 4
+	mov r0,#4
+	mov r1,#1
+	bl SetGpioFunction	
+	@@GPIO para escritura puerto 17
+	mov r0,#17
+	mov r1,#1
+	bl SetGpioFunction
+	@Display 2---------
+	@@GPIO para escritura puerto 22
+	mov r0,#22
+	mov r1,#1
+	bl SetGpioFunction
+	@@GPIO para escritura puerto 10
+	mov r0,#10
+	mov r1,#1
+	bl SetGpioFunction
+	@@GPIO para escritura puerto 9
+	mov r0,#9
+	mov r1,#1
+	bl SetGpioFunction
+	@@GPIO para escritura puerto 11
+	mov r0,#11
+	mov r1,#1
+	bl SetGpioFunction
+	@@Indicador de alarma (Buzzer/LED)
+	mov r0,#12		@Puerto 12 , como salida 
+	mov r1,#1
+	bl SetGpioFunction
+	/* Entradas */
+	@Button subir 
+	mov r0,#5  	@Puerto 5 como entrada 
+	mov r1,#0 
+	bl SetGpioFunction
+	@@Button cambiar digito
+	mov r0,#6	@Puerto 6 como entrada 
+	mov r1,#0
+	bl SetGpioFunction 
+	@@Button reset (Empezar alarma en 0)
+	mov r0,#13 	@Puerto 13 como entrada
+	mov r1,#0
+	bl SetGpioFunction
+
 
 start:
 	/* Mostramos el menu */
@@ -84,13 +160,7 @@ cronometro:
 	mov r0, cont
 	bl getDigits
 	@Mostramos los digitos en los displays
-	/*
 	bl numeros
-	*/
-	mov r2, r0
-	mov r1, r1
-	ldr r0, =testStr
-	bl printf
 	@Esperamos por un segundo
 	mov r0, #1
 	bl segundos
